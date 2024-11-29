4eecbe118e099ba327c2f65f71438adde9b416ad
Gekd <robinott@ut.ee>
2024-11-29 23:21:17 +0200

    fix: removed STREAMLIT

---
+++ andmetorm/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+  <button @click="counter++">Add 1</button>
+  <p>Count is: {{ counter }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
