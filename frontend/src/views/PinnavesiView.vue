ddececda6b4473694cf03d2f23f927e8f81a697c
OskarMannik <mannikoskar@gmail.com>
2024-11-30 04:30:18 +0200

    UI with Vue

---
+++ frontend/src/views/PinnavesiView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="content-container">
+    <ImageDisplay 
+      title="Pinnavee võtt" 
+      imagePath="../../data/image.png"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ImageDisplay from '../components/ImageDisplay.vue'
+</script>
+
+<style scoped>
+.content-container {
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style> 
